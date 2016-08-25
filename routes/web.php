d92119c5126fa0d996c9003ca0e3f347630d93d2
Nikita Kiselev <kiselev2008@gmail.com>
2016-08-25 18:52:07 +0300

    create page controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-Route::get('/', function () {
-    return view('pages.front');
-})->name('front');
+Route::get('/', 'PageController@front')->name('front');
 
 Route::get('/home', 'HomeController@index');
 
