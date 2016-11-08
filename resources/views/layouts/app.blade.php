c273b19e6ee984418471384d37cf1bbefaa06f1f
Nikita Kiselev <kiselev2008@gmail.com>
2016-11-08 14:20:41 +0300

    remove unused scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/min/dropzone.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css"/>
-    <link rel="stylesheet" href="{{ asset('gallery/css/style.css') }}"/>
 
     @if (Auth::check())
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropper/2.3.3/cropper.min.css"/>
@@ -44,7 +43,6 @@
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script src="{{ asset('gallery/js/modernizr.custom.53451.js') }}"></script>
     <script src="https://unpkg.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
     <script src="https://unpkg.com/masonry-layout@4.1/dist/masonry.pkgd.min.js"></script>
 
@@ -53,7 +51,6 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/cropper/2.3.3/cropper.min.js"></script>
     @endif
 
-    <script src="{{ asset('gallery/js/jquery.gallery.js') }}"></script>
     <script src="{{ elixir('js/app.js') }}"></script>
 
     @yield('scripts')
