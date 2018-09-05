ccd60c4f09a8b2c46f10c9ba8229b60a7d516ed7
Nikita Kiselev <kiselev2008@gmail.com>
2018-09-05 09:50:22 +0300

    Fix deploy script for staging

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,8 +3,8 @@
 @task('deploy', ['on' => 'staging'])
     cd /home/u2846/domains/staging.annalesnikova.ru
     git fetch origin
-    git checkout master
-    git pull origin master
+    git checkout develop
+    git reset --hard origin/develop
     composer install
     php artisan migrate
 @endtask
